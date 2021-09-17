b61b30f4b21c696219f90a80d8380d8f284914e5
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-09-17 13:25:15 +0300

    Performing toggle operations

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggle } from "../redux/todos/todosSlice";
 
 function TodoList() {
+  const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items);
 
   return (
@@ -10,7 +12,12 @@ function TodoList() {
       {items.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
-            <input className="toggle" type="checkbox" />
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
             <button className="destroy"></button>
           </div>
