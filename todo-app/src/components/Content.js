34eae725d5119b9aa74be66c4edb371226dd57cd
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-09-17 11:29:32 +0300

    Preparing the overview

---
+++ todo-app/src/components/Content.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import TodoList from "./TodoList";
+import ContentFooter from "./ContentFooter";
+
+function Content() {
+  return (
+    <>
+      <section className="main">
+        <input className="toggle-all" type="checkbox" />
+        <label htmlfor="toggle-all">Mark all as complete</label>
+
+        <TodoList />
+
+        <ContentFooter />
+      </section>
+    </>
+  );
+}
+
+export default Content;
