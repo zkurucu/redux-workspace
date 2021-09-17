445cdd0d1a3634e6b8d6c5d4dde5c7c0aa045e8c
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-09-17 13:56:10 +0300

    Delete operations

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import { toggle } from "../redux/todos/todosSlice";
+import { toggle, destroy } from "../redux/todos/todosSlice";
 
 function TodoList() {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items);
+
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(destroy(id));
+    }
+  };
 
   return (
     <ul className="todo-list">
@@ -19,7 +25,10 @@ function TodoList() {
               onChange={() => dispatch(toggle({ id: item.id }))}
             />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(item.id)}
+            ></button>
           </div>
         </li>
       ))}
