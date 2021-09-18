e8dd5e8918675bdd931579ed6a94a3d76656b18c
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-09-18 17:14:15 +0300

    Pulling data via API (Thunk Middleware)

--- todo-app/client/src/redux/todos/todosSlice.js
+++ todo-app/client/src/redux/todos/todosSlice.js
@@ -1,20 +1,20 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getTodosAsync = createAsyncThunk(
+  "todos/getTodosAsync",
+  async () => {
+    const res = await axios("http://localhost:7000/todos");
+    return res.data;
+  }
+);
 
 export const todosSlice = createSlice({
   name: "todos",
   initialState: {
-    items: [
-      {
-        id: "1",
-        title: "Learn React",
-        completed: true,
-      },
-      {
-        id: "2",
-        title: "Read a book",
-        completed: false,
-      },
-    ],
+    items: [],
+    isLoading: false,
+    error: null,
     activeFilter: "all",
   },
   reducers: {
@@ -48,6 +48,19 @@ export const todosSlice = createSlice({
     clearCompleted: (state) => {
       const filtered = state.items.filter((item) => item.completed === false);
       state.items = filtered;
+    },
+  },
+  extraReducers: {
+    [getTodosAsync.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [getTodosAsync.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.isLoading = false;
+    },
+    [getTodosAsync.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
     },
   },
 });
